09b936abc6affb6ceffd12fe0fda50edd91fb693
jomujin <jomujin@gmail.com>
2022-11-14 12:35:59 +0900

    branch_check_is_satisfied_condition_func에 서브 테이블 유무를 확인하는 기능을 추가하라

--- dags/create_kb_py_info.py
+++ dags/create_kb_py_info.py
@@ -9,6 +9,8 @@ from src.config.args import DEFAULT_ARGS
 from src.config.conn import CONN_AIRFLOW_TUTORIAL
 from src.py.slack_alert import SlackAlert
 from src.py.check_table import (
+    check_existed_table,
+    get_recent_partitiondate,
     check_is_satisfied_condition
 )
 
@@ -32,9 +34,17 @@ DAG_PY = DAG(
 )
 conn = CONN_AIRFLOW_TUTORIAL.get_alchmy_conn()
 
+def branch_check_existed_all_sub_table_func(kb_base_wk):
+    res_1 = check_existed_table(conn, 'kb', f'kb_complex_{kb_base_wk}')
+    res_2 = check_existed_table(conn, 'kb', f'kb_peongtype_{kb_base_wk}')
+    res_3 = check_existed_table(conn, 'kb', f'kb_price_{kb_base_wk}')
+    res_4 = check_existed_table(conn, 'kb', f'kb_complex_pnu_map_{kb_base_wk}')
+    if res_1 and res_2 and res_3 and res_4:
+        return True
+    return False
 
-def branch_check_is_satisfied_condition(**kwargs):
-    res = check_is_satisfied_condition(
+def branch_check_is_satisfied_condition_func(**kwargs):
+    res, kb_base_wk = check_is_satisfied_condition(
         conn=kwargs['conn'],
         schema_1=kwargs['schema_1'],
         table_1=kwargs['table_1'],
@@ -42,9 +52,11 @@ def branch_check_is_satisfied_condition(**kwargs):
         table_2=kwargs['table_2']
     )
     # 다음에 실행할 task_id 를 반환한다.
-    if res: # 조건을 만족하면 True
-        return 'test_continue_task'
-    else: # 조건을 불만족하면 False
+    if res: # 업데이트 조건을 만족하면 True
+        if branch_check_existed_all_sub_table_func(kb_base_wk): # 서브 테이블 유무 만족하면 True
+            return 'test_continue_task'
+        return 'test_stop_task' # 서브 테이블 유무 불만족하면 False
+    else: # 업데이트 조건을 불만족하면 False
         return 'test_stop_task'
 
 def continue_func():
@@ -54,12 +66,12 @@ def stop_func():
     return 'stop'
 
 branch_check_condition_op = BranchPythonOperator(
-    task_id="check_is_satisfied_condition",
-    python_callable=branch_check_is_satisfied_condition,
+    task_id="check_is_satisfied_condition_task",
+    python_callable=branch_check_is_satisfied_condition_func,
     op_kwargs={
         'conn': conn,
         'schema_1': 'kb',
-        'table_1': 'kb_complex',
+        'table_1': 'kb_price',
         'schema_2': 'kb',
         'table_2': 'kb_py_info'
     },
@@ -71,13 +83,16 @@ branch_check_condition_op = BranchPythonOperator(
 continue_op = PythonOperator(
     task_id='test_continue_task',
     python_callable=continue_func,
+    provide_context=True,
     dag=DAG_PY
 )
 
 stop_op = PythonOperator(
     task_id='test_stop_task',
     python_callable=stop_func,
+    provide_context=True,
     dag=DAG_PY
 )
 
 branch_check_condition_op >> [continue_op, stop_op]
+
