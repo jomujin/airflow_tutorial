bbd20dca165d6259ad2dfaa0a76853138918d204
jomujin <jomujin@gmail.com>
2022-11-15 13:15:14 +0900

    table download 관련 함수들을 download_table.py에 생성하라

---
+++ dags/src/py/download_table.py
@@ -0,0 +1,53 @@
+import os
+import asyncio
+from ..config.condition import DB_DIR
+from ..py.check_table import get_recent_partitiondate
+
+
+async def apg_copy_table_to_csv(db, schema, table, f=None):
+    conn = await db.get_apg_conn()
+    result = await conn.copy_from_table(
+        table,
+        output=f or f"{table}.csv",
+        format="csv",
+        schema_name=schema,
+        header=True,
+    )
+    print(result)
+    await conn.close()
+
+async def apg_copy_from_query_to_csv(db, table, sql, f):
+    conn = await db.get_apg_conn()
+    result = await conn.copy_from_query(
+        sql, 
+        output=f or f"{table}.csv", 
+        format="csv", 
+        header=True)
+    print(result)
+    await conn.close()
+
+def runApg(apg_func):
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(apg_func)
+    loop.close()
+
+def cacheTableToCSV(db, schema, table):
+    cache_file = f"{DB_DIR}/{table}.csv"
+    if not os.path.exists(cache_file):
+        with open(cache_file, "wb") as f:
+            runApg(apg_copy_table_to_csv(db, schema, table, f))
+
+def cacheTableToCSVByQuery(db, table, sql):
+    cache_file = f"{DB_DIR}/{table}.csv"
+    if not os.path.exists(cache_file):
+        with open(cache_file, "wb") as f:
+            runApg(apg_copy_from_query_to_csv(db, table, sql, f))
+
+def download_sub_table(db, schema, table):
+    conn = db.get_alchmy_conn()
+    kb_base_wk = get_recent_partitiondate(conn, schema, table)
+    cacheTableToCSV(
+        db=db,
+        schema=schema,
+        table=f'{table}_{kb_base_wk}'
+    )
