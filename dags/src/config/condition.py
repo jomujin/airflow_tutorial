d8de2e8d03c608db6b464f8754b78f27adedc01a
jomujin <jomujin@gmail.com>
2022-11-15 23:11:32 +0900

    condition에 dtypes을 추가하라

--- dags/src/config/condition.py
+++ dags/src/config/condition.py
@@ -1,4 +1,6 @@
 import os
+import sqlalchemy
+
 
 PATH = os.getcwd()
 DB_DIR = "db" 
@@ -8,9 +10,39 @@ DB_PATH = PATH + '/' + DB_DIR
 KB_SCHEMA = "kb"
 
 # TABLE
-KB_RAW_COMPLEX = "kb_complex"
-KB_RAW_PYTYPE = "kb_peongtype"
-KB_RAW_PRICE = "kb_price"
+KB_RAW_COMPLEX_TABLE = "kb_complex"
+KB_RAW_PYTYPE_TABLE = "kb_peongtype"
+KB_RAW_PRICE_TABLE = "kb_price"
 KB_COMPLEX_PNU_MAP_TABLE = "kb_complex_pnu_map"
 KB_PY_INFO_TABLE = "kb_py_info"
 
+KB_PY_INFO_SQL_DTYPES = {
+    'kb_complex_code' : sqlalchemy.types.Text(),
+    'kb_complex_name' : sqlalchemy.types.Text(),
+    'kb_approval_ym' : sqlalchemy.types.Text(),
+    'kb_occupancy_ym' : sqlalchemy.types.Text(),
+    'kb_type_code' : sqlalchemy.types.Text(),
+    'kb_area_jy' : sqlalchemy.types.Float(),
+    'kb_area_contract' : sqlalchemy.types.Float(),
+    'kb_etc_area_jy' : sqlalchemy.types.Text(),
+    'kb_pyeong_typ' : sqlalchemy.types.Text(),
+    'kb_pyeong_typ_main' : sqlalchemy.types.Integer(),
+    'kb_pyeong_typ_sub' : sqlalchemy.types.Text(),
+    'kb_rm_cnt' : sqlalchemy.types.Integer(),
+    'kb_brm_cnt' : sqlalchemy.types.Integer(),
+    'kb_dir' : sqlalchemy.types.Text(),
+    'kb_ent_strc' : sqlalchemy.types.Text(),
+    'kb_price' : sqlalchemy.types.Float(),
+    'kb_lower_price' : sqlalchemy.types.Float(),
+    'kb_upper_price' : sqlalchemy.types.Float(),
+    'kb_jeonse_price' : sqlalchemy.types.Float(),
+    'kb_lower_jeonse_price' : sqlalchemy.types.Float(),
+    'kb_upper_jeonse_price' : sqlalchemy.types.Float(),
+    'kb_mth_rent_deposit' : sqlalchemy.types.Float(),
+    'kb_lower_mth_rent_price' : sqlalchemy.types.Float(),
+    'kb_upper_mth_rent_price' : sqlalchemy.types.Float(),
+    'kb_base_dt' : sqlalchemy.types.Date(),
+    'kb_limit_dt' : sqlalchemy.types.Date(),
+    'kb_is_not_prov' : sqlalchemy.types.Integer(),
+    'de_base_wk' : sqlalchemy.types.Text()
+}
