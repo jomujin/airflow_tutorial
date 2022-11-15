cb812a3abdcfc4634c2ab65e8b6c5cc11f137de0
jomujin <jomujin@gmail.com>
2022-11-15 23:12:50 +0900

    condition의 변수명으로 테이블명을 수정하라

--- dags/dag_kb_py_info.py
+++ dags/dag_kb_py_info.py
@@ -7,6 +7,14 @@ from airflow.operators.python import (
 from datetime import datetime, timedelta
 from src.config.args import DEFAULT_ARGS
 from src.config.conn import CONN_AIRFLOW_TUTORIAL
+from src.config.condition import (
+    KB_SCHEMA,
+    KB_RAW_COMPLEX_TABLE,
+    KB_RAW_PYTYPE_TABLE,
+    KB_RAW_PRICE_TABLE,
+    KB_COMPLEX_PNU_MAP_TABLE,
+    KB_PY_INFO_TABLE
+)
 from src.py.helper.slack_helper import SlackAlert
 from src.py.helper.tb_check_helper import (
     check_existed_table,
@@ -21,6 +29,9 @@ from src.py.helper.xcom_helper import (
 )
 from src.py.creator.kb_py_info import (
     create_kb_py_info_table
+)
+from src.py.helper.tb_upload_helper import (
+    update_kb_py_info
 )
 
 
@@ -44,10 +55,10 @@ DAG_PY = DAG(
 conn = CONN_AIRFLOW_TUTORIAL.get_alchmy_conn()
 
 def branch_check_existed_all_sub_table_func(kb_base_wk):
-    res_1 = check_existed_table(conn, 'kb', f'kb_complex_{kb_base_wk}')
-    res_2 = check_existed_table(conn, 'kb', f'kb_peongtype_{kb_base_wk}')
-    res_3 = check_existed_table(conn, 'kb', f'kb_price_{kb_base_wk}')
-    res_4 = check_existed_table(conn, 'kb', f'kb_complex_pnu_map_{kb_base_wk}')
+    res_1 = check_existed_table(conn, KB_SCHEMA, f'{KB_RAW_COMPLEX_TABLE}_{kb_base_wk}')
+    res_2 = check_existed_table(conn, KB_SCHEMA, f'{KB_RAW_PYTYPE_TABLE}_{kb_base_wk}')
+    res_3 = check_existed_table(conn, KB_SCHEMA, f'{KB_RAW_PRICE_TABLE}_{kb_base_wk}')
+    res_4 = check_existed_table(conn, KB_SCHEMA, f'{KB_COMPLEX_PNU_MAP_TABLE}_{kb_base_wk}')
     if res_1 and res_2 and res_3 and res_4:
         return True
     return False
@@ -80,6 +91,10 @@ def pull_kb_base_wk_and_create_kb_py_info_table(**context):
     kb_base_wk = return_pull_xcom(task_ids='return_kb_base_wk_task', **context)
     create_kb_py_info_table(kb_base_wk)
 
+def pull_kb_base_wk_and_update_kb_py_info_table(conn, table, **context):
+    kb_base_wk = return_pull_xcom(task_ids='return_kb_base_wk_task', **context)
+    update_kb_py_info(conn, table, kb_base_wk)
+
 def stop_func():
     return 'stop'
 
@@ -87,11 +102,11 @@ branch_check_condition_op = BranchPythonOperator(
     task_id="check_is_satisfied_condition_task",
     python_callable=branch_check_is_satisfied_condition_func,
     op_kwargs={
-        'conn': conn,
-        'schema_1': 'kb',
-        'table_1': 'kb_price',
-        'schema_2': 'kb',
-        'table_2': 'kb_py_info'
+        'conn': CONN_AIRFLOW_TUTORIAL.get_alchmy_conn(),
+        'schema_1': KB_SCHEMA,
+        'table_1': KB_RAW_PRICE_TABLE,
+        'schema_2': KB_SCHEMA,
+        'table_2': KB_PY_INFO_TABLE
     },
     trigger_rule="all_done",
     provide_context=True,
@@ -102,9 +117,9 @@ return_kb_base_wk_op = PythonOperator(
     task_id='return_kb_base_wk_task',
     python_callable=return_kb_base_wk,
     op_kwargs={
-        'conn': conn,
-        'schema': 'kb',
-        'table': 'kb_price'
+        'conn': CONN_AIRFLOW_TUTORIAL.get_alchmy_conn(),
+        'schema': KB_SCHEMA,
+        'table': KB_RAW_PRICE_TABLE
     },
     provide_context=True,
     dag=DAG_PY
@@ -115,8 +130,8 @@ download_kb_complex_op = PythonOperator(
     python_callable=pull_kb_base_wk_and_download_sub_table,
     op_kwargs={
         'db': CONN_AIRFLOW_TUTORIAL,
-        'schema': 'kb',
-        'table': 'kb_complex',
+        'schema': KB_SCHEMA,
+        'table': KB_RAW_COMPLEX_TABLE,
     },
     trigger_rule="all_done",
     provide_context=True,
@@ -128,8 +143,8 @@ download_kb_peongtype_op = PythonOperator(
     python_callable=pull_kb_base_wk_and_download_sub_table,
     op_kwargs={
         'db': CONN_AIRFLOW_TUTORIAL,
-        'schema': 'kb',
-        'table': 'kb_peongtype',
+        'schema': KB_SCHEMA,
+        'table': KB_RAW_PYTYPE_TABLE,
     },
     trigger_rule="all_done",
     provide_context=True,
@@ -141,8 +156,8 @@ download_kb_price_op = PythonOperator(
     python_callable=pull_kb_base_wk_and_download_sub_table,
     op_kwargs={
         'db': CONN_AIRFLOW_TUTORIAL,
-        'schema': 'kb',
-        'table': 'kb_price',
+        'schema': KB_SCHEMA,
+        'table': KB_RAW_PRICE_TABLE,
     },
     trigger_rule="all_done",
     provide_context=True,
@@ -154,8 +169,8 @@ download_kb_complex_pnu_map_op = PythonOperator(
     python_callable=pull_kb_base_wk_and_download_sub_table,
     op_kwargs={
         'db': CONN_AIRFLOW_TUTORIAL,
-        'schema': 'kb',
-        'table': 'kb_complex_pnu_map',
+        'schema': KB_SCHEMA,
+        'table': KB_COMPLEX_PNU_MAP_TABLE,
     },
     trigger_rule="all_done",
     provide_context=True,
@@ -165,6 +180,18 @@ download_kb_complex_pnu_map_op = PythonOperator(
 create_kb_py_info_op = PythonOperator(
     task_id='create_kb_py_info_task',
     python_callable=pull_kb_base_wk_and_create_kb_py_info_table,
+    trigger_rule="all_done",
+    provide_context=True,
+    dag=DAG_PY
+)
+
+update_kb_py_info_op = PythonOperator(
+    task_id='update_kb_py_info_task',
+    python_callable=pull_kb_base_wk_and_update_kb_py_info_table,
+    op_kwargs={
+        'conn': CONN_AIRFLOW_TUTORIAL.get_alchmy_conn(),
+        'table': KB_PY_INFO_TABLE
+    },
     trigger_rule="all_done",
     provide_context=True,
     dag=DAG_PY
@@ -190,5 +217,5 @@ return_kb_base_wk_op >> [
     download_kb_peongtype_op,
     download_kb_price_op,
     download_kb_complex_pnu_map_op
-] >> create_kb_py_info_op >> finish_op
+] >> create_kb_py_info_op >> update_kb_py_info_op >> finish_op
 stop_op >> finish_op
