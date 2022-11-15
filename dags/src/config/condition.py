ba4ec8b32176991d07b9251d935bb95147b4c3fc
jomujin <jomujin@gmail.com>
2022-11-15 16:05:45 +0900

    condition에 스키마, 테이블명 정의하라

--- dags/src/config/condition.py
+++ dags/src/config/condition.py
@@ -3,3 +3,14 @@ import os
 PATH = os.getcwd()
 DB_DIR = "db" 
 DB_PATH = PATH + '/' + DB_DIR
+
+# SCHEMA
+KB_SCHEMA = "kb"
+
+# TABLE
+KB_RAW_COMPLEX = "kb_complex"
+KB_RAW_PYTYPE = "kb_peongtype"
+KB_RAW_PRICE = "kb_price"
+KB_COMPLEX_PNU_MAP_TABLE = "kb_complex_pnu_map"
+KB_PY_INFO_TABLE = "kb_py_info"
+
